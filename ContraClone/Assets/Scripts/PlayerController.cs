3466116410bb54849155712172e19684967788fe
Joseph Tomasso <jatboygriefs@gmail.com>
2018-11-02 01:03:37 -0400

    Basic Start
    Added basic movement, test scene, textures for testing, added animations

---
+++ ContraClone/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour {
+    public Animator animator;
+    public CharacterController2D controller;
+
+    public float runSpeed = 40f;
+
+    float horizontalMove = 0f;
+    bool jump = false;
+    bool crouch = false;
+
+    private void Update() {
+        horizontalMove = Input.GetAxisRaw("Horizontal") * runSpeed;
+
+        animator.SetFloat("MoveSpeed", Mathf.Abs(horizontalMove));
+
+        if (Input.GetButtonDown("Jump")) {
+            jump = true;
+            animator.SetBool("IsJumping", true);
+        }
+
+    }
+
+    private void FixedUpdate() {
+        controller.Move(horizontalMove * Time.fixedDeltaTime, crouch, jump);
+        jump = false;
+    }
+
+    public void OnLand() {
+        animator.SetBool("IsJumping", false);
+    }
+
+}
